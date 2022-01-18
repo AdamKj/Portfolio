5efcef125a09a8412542bbff0585b7fda2a476cb
Adam Kjellberg <76393729+AdamKj@users.noreply.github.com>
2022-01-18 09:53:28 +0100

    ready to work on

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Home = () => 
+{
+    return (
+        <>
+    <div className="header">
+        <h1>HELLO</h1>
+        <p>HELLO</p>
+        <p>HELLO</p>
+        <p>HELLO</p>
+    </div>
+        </>
+    )
+}
